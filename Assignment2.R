1637b21a05ca598baa7370f014115113e18052cd
tiburanadrian7-ship-it <tiburanadrian7@gmail.com>
2025-09-15 19:58:58 +0800

    Update all files with latest changes

--- Assignment2.R
+++ Assignment2.R
@@ -1,12 +1,12 @@
 score <- 72
 if(score<=100 & score>=90){
-  print("GRADE: A")
-} else if(score<=89 & score>=90){
-  print("GRADE: B")
+  print("GRADE:A")
+} else if(score<=89 & score>=80){
+  print("GRADE:B")
 } else if(score<=79 & score>=70){
-  print("GRADE: C")
+  print("GRADE:C")
 } else if(score<=69 & score>=60){
-  print("GRADE: D")
-} else if(score<=60){
-  print("GRADE: F")
+  print("GRADE:D")
+} else if(score<60){
+  print("GRADE:F")
 }
